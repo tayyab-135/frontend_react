1d1b83719dca7a1506fef855881e421e8267f9c1
Muhammad Tayyab <programmer.tayyab@gmail.com>
2022-06-10 00:47:55 +0500

    login age

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-import About from './AboutUs/About';
-import './App.css';
-import Footer from './Components/Footer';
-import NavBar from './Components/NavBar';
-import login from './LoginPage/login';
-import Home from './Home/Home';
-import Register from './Registration/Register';
-import bgImage from './Video/Background-img.mp4';
-import {
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import Login from './LoginPage/login';
-import TeacherDashboard from './TeacherDashboard/TeacherDashboard';
-import StudentDashboard from './StudentDashboard/StudentDashboard';
+import About from "./AboutUs/About";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+
+import "./App.css";
+import Footer from "./Components/Footer";
+import NavBar from "./Components/NavBar";
+import login from "./LoginPage/login";
+import Home from "./Home/Home";
+import Register from "./Registration/Register";
+import bgImage from "./Video/Background-img.mp4";
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./LoginPage/login";
+import TeacherDashboard from "./TeacherDashboard/TeacherDashboard";
+import StudentDashboard from "./StudentDashboard/StudentDashboard";
+import AlertTemplate from "react-alert-template-basic";
+
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: transitions.SCALE,
+};
 
 const App = () => {
-  const style={
-    fontSize : "22px",
-    FontFamily : "Courier New",
-  }
+  const style = {
+    fontSize: "22px",
+    FontFamily: "Courier New",
+  };
   return (
-     <>
-       <NavBar/>
-       <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/Contact" element={<About />}/>
-          <Route path="/Registration" element={<Register/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/StudentDashboard" element={< StudentDashboard/>}/>
-          <Route path="/TeacherDashboard" element={< TeacherDashboard/>}/>
+    <>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<About />} />
+          <Route path="/Registration" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/StudentDashboard" element={<StudentDashboard />} />
+          <Route path="/TeacherDashboard" element={<TeacherDashboard />} />
         </Routes>
-       <Footer/> 
-     </>
+        <Footer />
+      </AlertProvider>
+    </>
   );
-}
+};
 
 export default App;
--- src/LoginPage/login.js
+++ src/LoginPage/login.js
@@ -1,44 +1,91 @@
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useAlert } from "react-alert";
+import { validateEmail } from "../utils/validators";
 
 export default function Login() {
-  const [email,setEmail]=useState('');
-  const [password,setPassword]=useState('');
+  const alert = useAlert();
 
-  const onLogin=()=>{
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  }
-    return (
-        
-        <>
+  const onLogin = (e) => {
+    e.preventDefault();
 
-<div className="container my-3">
-         <div><center><img my-5 src="login.png" className="animated bounce infinite my-4" id="animated-img1" width="50%" alt="..."/></center></div>
-        
+    if (!email || !password)
+      return alert.error("email and password cannot be emty");
+    if (!validateEmail(email)) {
+      alert.error("invalid email!");
+    }
+    if (password.length < 8) {
+      alert.error("password must be greator than 8 characters");
+    }
+  };
+  return (
+    <>
+      <div className="container my-3">
+        <div>
+          <center>
+            <img
+              my-5
+              src="login.png"
+              className="animated bounce infinite my-4"
+              id="animated-img1"
+              width="50%"
+              alt="..."
+            />
+          </center>
         </div>
-        <div className="container">
-            <center><h1><i>Welcome, Login Here</i></h1></center>
-            <hr/><br/><br/>
-            <form className="needs-validation row g-3" novalidate>
-  
-  <div className="col-md-8">
-    <label for="inputEmail"  className="form-label"><strong>Email</strong></label>
-    <input id="email" type="email"  className="form-control" placeholder="Enter Email" name="email" id="inputEmail" required/>
-  </div>
-  
-  <div className="col-md-8">
-    <label for="inputCity" className="form-label"><strong>Password</strong></label>
-    <input type="password" className="form-control" id="inputCity" required/>
-  </div>
-  <div className="col-12 my-4">
-    <button type="submit" className="btn btn-info">Login</button>
-  </div>
-  
-</form>
+      </div>
+      <div className="container">
+        <center>
+          <h1>
+            <i>Welcome, Login Here</i>
+          </h1>
+        </center>
+        <hr />
+        <br />
+        <br />
+        <form className="needs-validation row g-3" novalidate>
+          <div className="col-md-8">
+            <label for="inputEmail" className="form-label">
+              <strong>Email</strong>
+            </label>
+            <input
+              id="email"
+              onChange={(evt) => setEmail(evt.target.value)}
+              value={email}
+              type="email"
+              className="form-control"
+              placeholder="Enter Email"
+              name="email"
+            />
+          </div>
 
-
-    </div> 
-  
-
-        </>
-    )
+          <div className="col-md-8">
+            <label for="inputCity" className="form-label">
+              <strong>Password</strong>
+            </label>
+            <input
+              value={password}
+              type="password"
+              className="form-control"
+              onChange={(evt) => setPassword(evt.target.value)}
+              id="inputCity"
+              required
+            />
+          </div>
+          <div className="col-12 my-4">
+            <button
+              onClick={(e) => onLogin(e)}
+              // type="submit"
+              className="btn btn-info"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
